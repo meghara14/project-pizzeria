06ca566d2ac7061d0882d86fd97cf234e6ec127a
Magdalena Lesisz <meghara@icloud.com>
2023-09-15 21:31:52 +0200

    dodawanie obrazków

--- src/js/script.js
+++ src/js/script.js
@@ -92,6 +92,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -159,7 +160,10 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
+          if (optionImage != null) {
+            optionImage.classList.remove(classNames.menuProduct.imageVisible);
+          }
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
@@ -167,12 +171,20 @@
               // add option price to price variable
               price += option.price;
             }
+            if (optionImage) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+
           } else {
             // check if the option is default
             if (option.default) {
               // reduce price variable
               price -= option.price;
+              if (optionImage) {
+                optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              }
             }
+
           }
 
         }
