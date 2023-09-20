a52f91e16eb4d8d61a316d04f06fdd2224a91f4c
Magdalena Lesisz <meghara@icloud.com>
2023-09-20 19:34:01 +0200

    koszyk aktualizacja

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -371,6 +376,8 @@
 
       thisCart.products = [];
       thisCart.dom = {};
+      this.totalNumber = 0;
+      this.subtotalPrice = 0;
 
       thisCart.getElements(element);
       thisCart.initActions();
@@ -387,6 +394,9 @@
         subtotalPrice: element.querySelector(select.cart.subtotalPrice),
         totalPrice: element.querySelectorAll(select.cart.totalPrice),
         totalNumber: element.querySelector(select.cart.totalNumber),
+        form: element.querySelector(select.cart.form),
+        address: element.querySelector(select.cart.address),
+        phone: element.querySelector(select.cart.phone),
       };
     }
     initActions() {
@@ -396,15 +406,20 @@
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
-    
+
       thisCart.dom.productList.addEventListener('updated', function () {
         thisCart.update();
       });
-    
+
       thisCart.dom.productList.addEventListener('remove', function (event) {
         event.preventDefault();
         const cartProduct = event.detail.cartProduct;
         thisCart.remove(cartProduct);
+      });
+
+      thisCart.dom.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisCart.sentOrder();
       });
 
     }
@@ -420,46 +435,83 @@
     }
     update() {
       const thisCart = this;
-    
+
       thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.totalNumber = 0;
       thisCart.subtotalPrice = 0;
-    
+
       for (let singleProduct of thisCart.products) {
         thisCart.totalNumber += singleProduct.amount;
         thisCart.subtotalPrice += singleProduct.price;
       }
-    
+
       thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
-    
+
       // Jeśli w koszyku nie ma produktów, koszt dostawy wynosi zero
       if (thisCart.totalNumber === 0) {
         thisCart.deliveryFee = 0;
       }
-    
+
       // Aktualizacja HTML koszyka
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
       thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
       thisCart.dom.deliveryFee.innerHTML = thisCart.deliveryFee;
-    
+
       for (let totalPriceElem of thisCart.dom.totalPrice) {
         totalPriceElem.innerHTML = thisCart.totalPrice;
       }
     }
     remove(cartProduct) {
       const thisCart = this;
-    
+
       // Remove the product representation from HTML
       cartProduct.dom.wrapper.remove();
-    
+
       // Find and remove the product from the products array
       const productIndex = thisCart.products.indexOf(cartProduct);
       if (productIndex !== -1) {
         thisCart.products.splice(productIndex, 1);
       }
-    
+
       // Recalculate totals
       thisCart.update();
+    }
+
+    sentOrder() {
+      const thisCart = this;
+      const url = settings.db.url + '/' + settings.db.orders;
+    
+      const payload = {
+        address: thisCart.dom.address.value,
+        phone: thisCart.dom.phone.value,
+        totalPrice: thisCart.totalPrice,
+        subtotalPrice: thisCart.subtotalPrice,
+        totalNumber: thisCart.totalNumber,
+        deliveryFee: thisCart.deliveryFee,
+        products: [],
+      };
+    
+      for (let prod of thisCart.products) {
+        console.log('Product:', prod);
+        console.log('Has getData method:', 'getData' in prod);
+        payload.products.push(prod.getData());
+      }
+    
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+    
+      fetch(url, options)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+        });
     }
   }
 
@@ -473,10 +525,12 @@
       thisCartProduct.priceSingle = menuProduct.priceSingle;
       thisCartProduct.price = menuProduct.price;
       thisCartProduct.params = menuProduct.params;
+  
 
       thisCartProduct.getElements(element);
       this.initAmountWidget(this.amount);
       this.initActions();
+      
     }
 
     getElements(element) {
@@ -508,12 +562,12 @@
 
     initActions() {
       const thisCartProduct = this;
-    
+
       thisCartProduct.dom.edit.addEventListener('click', function (event) {
         event.preventDefault();
         // Add your edit logic here
       });
-    
+
       thisCartProduct.dom.remove.addEventListener('click', function (event) {
         event.preventDefault();
         const removeEvent = new CustomEvent('remove', {
@@ -536,7 +590,20 @@
       });
       thisCartProduct.dom.wrapper.dispatchEvent(event);
     }
+    getData() {
+      const thisCartProduct = this;
 
+      const data = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        name: thisCartProduct.name,
+        params: thisCartProduct.params,
+      };
+
+      return data;
+    }
   }
 
   const app = {
@@ -544,14 +611,28 @@
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+        .then(function (rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function (parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+          /* save ParsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /*execute initMenu method */
+          thisApp.initMenu();
+        });
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function () {
@@ -570,7 +651,6 @@
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
       console.log("koszyk:", thisApp.cart);
     },
