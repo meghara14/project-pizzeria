b91664b2aaeca608ec28c13ae81972223b2c84fd
Magdalena Lesisz <meghara@icloud.com>
2023-10-02 14:19:32 +0200

    ready to public

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -5,8 +5,6 @@ class Home {
     const thisHome = this;
 
     thisHome.render(element);
-    thisHome.initWidgets();
-
   }
 
   render(element) {
@@ -17,18 +15,7 @@ class Home {
     thisHome.dom = {};
     thisHome.dom.wrapper = element;
     thisHome.dom.wrapper.innerHTML = generatedHTML;
-
-    // Wyszukaj przyciski bez użycia stałej select
-    thisHome.dom.orderButton = thisHome.dom.wrapper.querySelector('.home-button a[href="#order"]');
-    thisHome.dom.bookButton = thisHome.dom.wrapper.querySelector('.home-button a[href="#booking"]');
   }
-
-  initWidgets() {
-    // Inicjowanie karuzeli lub innych pluginów, jeśli to konieczne
-    // Na przykład, używając odpowiednich bibliotek do karuzeli
-    // np. Slick Carousel lub Swiper
-  }
-
 }
 
 export default Home;
